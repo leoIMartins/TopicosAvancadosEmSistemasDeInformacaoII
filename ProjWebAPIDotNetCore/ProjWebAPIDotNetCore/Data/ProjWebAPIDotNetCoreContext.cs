df57cbd1bfc0809bed3d52d7ef54f3f11a0db5ff
leoIMartins <leonardo_ignacio2010@hotmail.com>
2021-08-30 21:10:56 -0300

    projeto API dot net core

---
+++ ProjWebAPIDotNetCore/ProjWebAPIDotNetCore/Data/ProjWebAPIDotNetCoreContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjWebAPIDotNetCore.Model;
+
+namespace ProjWebAPIDotNetCore.Data
+{
+    public class ProjWebAPIDotNetCoreContext : DbContext
+    {
+        public ProjWebAPIDotNetCoreContext (DbContextOptions<ProjWebAPIDotNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjWebAPIDotNetCore.Model.Cliente> Cliente { get; set; }
+
+        public DbSet<ProjWebAPIDotNetCore.Model.Padaria> Padaria { get; set; }
+
+        public DbSet<ProjWebAPIDotNetCore.Model.Produto> Produto { get; set; }
+    }
+}
